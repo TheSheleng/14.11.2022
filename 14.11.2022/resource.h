a385afee13dc850f5e64b35a6e92e362b269e718
TheSheleng <sheleng55@gmail.com>
2022-11-15 00:19:31 +0200

    1

--- 14.11.2022/resource.h
+++ 14.11.2022/resource.h
@@ -12,6 +12,19 @@
 #define IDC_RADIO1                      1004
 #define IDC_RADIO2                      1005
 #define IDC_EDIT1                       1006
+#define IDC_SPIN1                       1007
+#define IDC_SPIN2                       1008
+#define IDC_EDIT2                       1009
+#define IDC_SPIN3                       1010
+#define IDC_EDIT3                       1011
+#define IDC_SPIN4                       1012
+#define IDC_EDIT4                       1013
+#define IDC_SPIN5                       1014
+#define IDC_EDIT5                       1015
+#define IDC_SPIN6                       1016
+#define IDC_EDIT6                       1017
+#define IDC_SPIN7                       1020
+#define IDC_EDIT7                       1021
 
 // Next default values for new objects
 // 
@@ -19,7 +32,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
